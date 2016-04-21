4ef59088bd5aa4b546991b15c3015816edbbc6c8
gspohu <pohuvalentin@gmail.com>
2016-04-21 13:37:39 +0200

    Placement logo CSS

--- view/nav/nav.php
+++ view/nav/nav.php
@@ -1,11 +1,11 @@
 <div class="nav">
-	<a href="index.php"><img src="images/logo.png" alt="Logo" /></a>
+	<a href="index.php"><img class="nav_logo" src="images/logo.png" alt="Logo" /></a>
 	<a class="nav_link" href="open-hardware.php"> Open-Hardware </a>
 	<a class="nav_link" href="tools.php"> Tools </a>
 	<a class="nav_link" href="pricing.php"> Princing </a>
 	
 	<form method="post" action="searchbar.php">
-        	<label for="Search"></label><input class="searchbar" type="text" name="searchbar" id="searchbar" placeholder="Rechercher" />
+        	<label for="Search"></label><input type="text" name="searchbar" id="searchbar" placeholder="Rechercher" />
         </form>
 
 	<div class="bouton_connexion">
