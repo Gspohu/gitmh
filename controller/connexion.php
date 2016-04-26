69360608fd18269ddd7e6da71906bec090ad8cf1
gspohu <pohuvalentin@gmail.com>
2016-04-26 17:08:57 +0200

    Mise en place SQL de la connexion

--- controller/connexion.php
+++ controller/connexion.php
@@ -5,5 +5,31 @@ include_once('view/nav/nav.php');
 
 include_once('view/body/connexion.php');
 
+if (isset($_POST['conn_pseudo']) && isset($_POST['conn_password']))
+{
+	$pseudo= $_POST['conn_pseudo'];
+	$password = hash('sha256', $_POST['conn_password']);
+
+	// Vérification des identifiants
+	$req = $bdd->prepare('SELECT id FROM Member WHERE Pseudo = :pseudo AND Password = :pass');
+	$req->execute(array(
+	    'pseudo' => $pseudo,
+	    'pass' => $password));
+
+	$result = $req->fetch();
+
+	if (!$result)
+	{
+	    echo 'Mauvais identifiant ou mot de passe !';
+	}
+	else
+	{
+	    session_start();
+	    $_SESSION['id'] = $resultat['id'];
+	    $_SESSION['pseudo'] = $pseudo;
+	    echo 'Vous êtes connecté !';
+	}
+}
+
 include_once('view/footer/footer.php');
 ?>
--- view/body/connexion.php
+++ view/body/connexion.php
@@ -4,8 +4,8 @@
 		<p class="title_connexion" >Sign up</p>
 
                 <form method="post" >
-                        <p class="title_form_connexion" >Username<p> <p><label for="Pseudo"></label> <input class="connexion_field" type="text" name="reg_pseudo" id="Pseudo" value="<?php echo htmlspecialchars($_POST['reg_pseudo']);?>" /></p>
-                        <p class="title_form_connexion" >Passphrase<p> <p><label for="Mot de passe"></label> <input class="connexion_field" type="password" name="reg_password" id="Mot de passe" /></p>
+                        <p class="title_form_connexion" >Username<p> <p><label for="Pseudo"></label> <input class="connexion_field" type="text" name="conn_pseudo" id="Pseudo" value="<?php echo htmlspecialchars($_POST['conn_pseudo']);?>" /></p>
+                        <p class="title_form_connexion" >Passphrase<p> <p><label for="Mot de passe"></label> <input class="connexion_field" type="password" name="conn_password" id="Mot de passe" /></p>
 			<a class="text_connexion" href="forgot_passphrase.php" >Forgot passphrase ?</a>
 		        <input class="submit_connexion" type="submit" value="Submit" />
 		</form>
