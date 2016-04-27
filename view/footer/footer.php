bc5fe71a339e3ade1c07d2759ab393ff2094490a
gspohu <pohuvalentin@gmail.com>
2016-04-27 18:11:12 +0200

    Ajout des liens au footer

--- view/footer/footer.php
+++ view/footer/footer.php
@@ -1,13 +1,13 @@
 <footer>
         <div class="copyright">
-		<div class="copyright_text">GPLv3 Cairn-devices 2016</div>
+		<div class="copyright_text"><a href="https://cairngit.eu/cairn-devices/cairngit">GPLv3 Cairn-devices 2016</a></div>
 	</div>
 
         <div class="links_footer">
-		<a href="index.php"><img class="footer_logo" src="images/logo_alpha.png" alt="Logo" /></a>
-                <div class="text_footer"><p>Map</p> <p>Cairn Devices</p> <p>Legal</p></div>
-		<div class="text_footer"><p>Privacy</p> <p>Language</p> <p>News</p></div>
-		<div class="text_footer"><p>Contact</p> <p>Support</p> <p>Press</p></div>
+		<a href="https://cairn-devices.eu"><img class="footer_logo" src="images/logo_alpha.png" alt="Logo" /></a>
+                <div class="text_footer"><a href="map.php">Map</a><br/> <a href="https://cairn-devices.eu">Cairn Devices</a><br/> <a href="legal.php">Legal</a><br/></div>
+		<div class="text_footer"><a href="privacy.php">Privacy</a><br/> <a href="language.php">Language</a><br/> <a href="news.php">News</a><br/></div>
+		<div class="text_footer"><a href="contact.php">Contact</a><br/> <a href="support.php">Support</a><br/> <a href="press.php">Press</a><br/></div>
         </div>
 
 </footer>
