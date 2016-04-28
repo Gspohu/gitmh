54d6b8c5fbbec9f9a5e1d163eb62ad78f1a4e310
gspohu <pohuvalentin@gmail.com>
2016-04-28 14:18:40 +0200

    Mise en place du captcha sur le formulaire d'inscription

--- controller/inscription.php
+++ controller/inscription.php
@@ -3,6 +3,11 @@ include_once('model/get_text.php');
 
 include_once('view/nav/nav.php');
 
+#Génération du captcha
+$nb_captcha=rand(1, 60);
+$captcha_name="images/captcha/captcha".$nb_captcha.".png";
+copy($captcha_name, 'images/captcha/captcha.png');
+
 include_once('view/body/inscription.php');
 
 if(isset($_POST['reg_pseudo']) && isset($_POST['reg_mail']) && isset($_POST['reg_password']))
--- view/body/inscription.php
+++ view/body/inscription.php
@@ -5,7 +5,8 @@
                         <p class="title_form_inscription" >Username<p> <p><label for="Pseudo"></label> <input class="inscription_field" type="text" name="reg_pseudo" id="Pseudo" value="<?php echo htmlspecialchars($_POST['reg_pseudo']);?>" /></p>
                         <p class="title_form_inscription" >Passphrase<p> <p><label for="Mot de passe"></label> <input class="inscription_field" type="password" name="reg_password" id="Mot de passe" /></p>
 			<p class="title_form_inscription" >Re-passphrase<p> <p><label for="ReMot de passe"></label> <input class="inscription_field" type="password" name="reg_repassword" id="ReMot de passe" /></p>
-                        <p class="title_form_inscription" >Email<p> <p><label for="Mail"></label> <input class="inscription_field" type="text" name="reg_mail" id="Mail" value="<?php echo htmlspecialchars($_POST['reg_mail']);?>"/></p>
+                        <p class="title_form_inscription" >Email<p> <p><label for="Mail"></label> <input class="inscription_field" type="email" name="reg_mail" id="Mail" value="<?php echo htmlspecialchars($_POST['reg_mail']);?>"/></p>
+			<p class="title_form_inscription" >Captcha<p> <p><label for="captcha"></label> <input class="captcha_field" type="text" name="captcha" id="captcha" placeholder="Copy the text to your left" /><div class="captcha" ></div></p>
 		        <input class="submit_inscription" type="submit" value="Submit" />
 		</form>
 
